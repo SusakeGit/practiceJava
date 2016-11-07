759ee900bd1bb3e4c09951a8e47c4fde359c9995
ragrok <ragrok.lee@hotmail.com>
2016-11-07 11:16:36 +0800

    add thinking in java

---
+++ Test/src/cn/com/github4/CarDemo.java
@@ -0,0 +1,31 @@
+package cn.com.github4;
+
+import cn.com.java2.static_final;
+
+class Car{
+	int num;
+	String color;
+	void run(){
+		System.out.println(num+"|"+color);
+	}
+}
+
+public class CarDemo {
+     public static void main(String[] args) {
+		Car c1 = new Car();
+		Car c2 = new Car();
+		
+		c1.run();
+		show(c2);
+		show(c1);
+		show(new Car());
+		
+	}
+     
+     public static void show(Car c){
+    	 System.out.println(c);
+    	 c.num = 4;
+    	 c.color = "black";
+    	 c.run();
+     }
+}
--- learnServlet/src/main/java/cn/com/github/HelloServlet.java
+++ learnServlet/src/main/java/cn/com/github/HelloServlet.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -31,12 +33,55 @@ public class HelloServlet extends HttpServlet {
 		out.println("<h1>Hello,这是一个 由Servlet 动态生成的网页！</h1>");
 		out.println("</body>");
 		out.println("</html>");
+		out.close();
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// handler get method style
 		System.out.println("Post requirement");
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(req, resp);
+	}
+
+	@Override
+	protected long getLastModified(HttpServletRequest req) {
+		// TODO Auto-generated method stub
+		return super.getLastModified(req);
+	}
+
+	@Override
+	protected void doHead(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doHead(req, resp);
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPut(req, resp);
+	}
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doDelete(req, resp);
+	}
+
+	@Override
+	protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doOptions(req, resp);
+	}
+
+	@Override
+	protected void doTrace(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doTrace(req, resp);
 	}
 
 }
