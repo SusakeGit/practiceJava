b990517e9d6a1c7b7d3bc5c6c64b5be032cf5d8f
ragrok <ragrok.lee@hotmail.com>
2016-12-27 12:39:19 +0800

    update project

---
+++ Test/src/cn/com/github/collection/Test.java
@@ -0,0 +1,18 @@
+package cn.com.github.collection;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class Test {
+	
+	private static long startTime() {
+		return System.currentTimeMillis();  
+	}
+
+	private static long endTime() {
+		return System.currentTimeMillis();
+	}
+	
+	private static Collection<String> arrayList = new LinkedList<String>();
+
+}
